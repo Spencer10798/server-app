b1711232d5014ea97a06b88b0a7c8fef25dcbf0d
Spencer10798 <maghrouri@hotmail.com>
2022-03-26 16:24:16 -0400

    DB Integration

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+let mongoose = require('mongoose')
+
 const app = express();
-const port = 3000;
+const port = 3006;
+
+let mongoDB = 'mongodb://localhost:27017/InstagramCloneDB';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
